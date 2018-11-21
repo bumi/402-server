70cd3ea84c3643a9190874a4c1f1d646fd0d2faf
bumi <hello@michaelbumann.com>
2018-11-21 21:05:11 +0100

    Add real rack lightning option

--- fourohtwo.rb
+++ fourohtwo.rb
@@ -5,9 +5,13 @@ Fourohtwo = Rack::Builder.new {
   map "/" do
     run Proc.new { |env| ['200', {'Content-Type' => 'text/html'}, ['Hello Hello']] }
   end
-  map "/402" do
+  map "/402-static" do
     # use Rack::Lightning
     run Proc.new { |env| ['402', {'Content-Type' => 'application/vnd.lightning.bolt11'}, ['invoice goes here']] }
+  end
+  map "/402" do
+    use Rack::Lightning, { credentials: ENV['LND_CREDENTIALS'], macaroon: ENV['LND_MACAROON'], address: ENV['LND_ADDRESS'] }
+    run Proc.new { |env| ['402', {'Content-Type' => 'text/html'}, ['thank you lightning!']] }
   end
 }.to_app
 
