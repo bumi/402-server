3842c99212e2ed551db7b725b74c22726cc46e4c
Michael Bumann <hello@michaelbumann.com>
2021-04-05 17:57:26 +0200

    Use lnrpc

--- fourohtwo.rb
+++ fourohtwo.rb
@@ -1,17 +1,63 @@
 require "bundler/setup"
+require 'sinatra'
 require "rack/lightning"
 
-Fourohtwo = Rack::Builder.new {
-  map "/" do
-    run Proc.new { |env| ['200', {'Content-Type' => 'text/html'}, ['Hello Hello <a href="/402">invoice</a>']] }
-  end
-  map "/402-static" do
-    # use Rack::Lightning
-    run Proc.new { |env| ['402', {'Content-Type' => 'application/vnd.lightning.bolt11'}, ['invoice goes here']] }
-  end
-  map "/402" do
-    use Rack::Lightning, { credentials: ENV['LND_CREDENTIALS'], macaroon: ENV['LND_MACAROON'], address: ENV['LND_ADDRESS'] }
-    run Proc.new { |env| ['402', {'Content-Type' => 'text/html'}, ['thank you lightning!']] }
-  end
-}.to_app
 
+LND = Lnrpc::Client.new({
+  credentials: ENV['LND_CREDENTIALS'],
+  macaroon: ENV['LND_MACAROON'],
+  address: ENV['LND_ADDRESS']
+})
+
+set :port, 4568
+get '/' do
+  erb :index
+end
+
+get '/402-html' do
+  invoice_res = LND.lightning.add_invoice(value: 100, memo: 'I :heart: ruby')
+  puts invoice_res.payment_request
+  #status 402
+  headers 'Content-Type' => 'text/html', 'WWW-Authenticate' => "lsat macaroon=\"hallo\" invoice=\"#{invoice_res.payment_request}\""
+  erb :invoice, locals: { invoice: invoice_res }
+end
+
+__END__
+
+@@ layout
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>402</title>
+    <meta charset="utf-8" />
+  </head>
+  <body><%= yield %></body>
+</html>
+
+@@ invoice
+<pre><a href="lightning:<%= invoice.payment_request %>">lnbtc...</a></pre>
+
+<script
+  src="https://unpkg.com/webln@0.2.0/dist/webln.min.js"
+  integrity="sha384-mTReBqbhPO7ljQeIoFaD1NYS2KiYMwFJhUNpdwLj+VIuhhjvHQlZ1XpwzAvd93nQ"
+  crossorigin="anonymous"
+></script>
+<script>
+window.addEventListener('DOMContentLoaded', function(event) {
+  window.setTimeout(function() {
+
+    WebLN.requestProvider()
+      .then(function(webln) {
+        return webln.sendPayment('<%= invoice.payment_request %>')
+          .then(function(r) {
+            console.log('done', r);
+          })
+          .catch(function(e) {
+            console.log('err', e);
+          });
+    })
+    .catch(function(e) { console.log('err, provider', e) });
+
+  }, 300);
+});
+</script>
